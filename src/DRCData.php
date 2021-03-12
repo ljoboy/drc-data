5de55eaff51a594df82b84d7bd2df70fc61a16c0
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 18:42:21 +0200

    DRCData main class

---
+++ src/DRCData.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FILE DRCData.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/12/2021 - 4:18 PM
+ *
+ * @package DRCData;
+ */
+
+namespace DRCData;
+
+
+class DRCData
+{
+    public function __construct()
+    {
+    }
+}
