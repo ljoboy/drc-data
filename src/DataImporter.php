bdc93bb6a4ed0d670f7ef76a2bb990d620b89217
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 18:14:16 +0200

    import territoires's data

--- src/DataImporter.php
+++ src/DataImporter.php
@@ -13,11 +13,25 @@ namespace DRCData;
 
 class DataImporter
 {
+    /**
+     * @return array
+     */
     public function provinces(): array
     {
         $provincesFile = file_get_contents('../data/provinces.json');
         $provinces = json_decode($provincesFile, true);
 
         return $provinces['administrative'] ?? [];
+    }
+
+    /**
+     * @return array
+     */
+    public function territoires(): array
+    {
+        $territoiresFile = file_get_contents('../data/territoires.json');
+        $territoires = json_decode($territoiresFile, true);
+
+        return $territoires['pcode_territoire'] ?? [];
     }
 }
