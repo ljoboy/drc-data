a08fbb7354bc23c74c8f3817e35c424cb170401a
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 22:57:07 +0200

    new Ville class

---
+++ src/Ville.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FILE Ville.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/12/2021 - 10:56 PM
+ *
+ * @package DRCData;
+ */
+
+namespace DRCData;
+
+
+class Ville
+{
+    public function __construct()
+    {
+    }
+}
