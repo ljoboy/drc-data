3ab8e468771bdcfa0a21552dc435e834d4f0e7fe
Dark Angel <jonathanyombo@gmail.com>
2021-03-13 01:10:53 +0200

    ZoneSante's class

---
+++ src/ZoneSante.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * FILE ZoneSante.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/13/2021 - 12:48 AM
+ *
+ * @package DRCData;
+ */
+
+namespace DRCData;
+
+
+class ZoneSante extends Data
+{
+
+}
