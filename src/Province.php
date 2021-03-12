e8fe8d4d8825076d0453b1fefba71c7dbd1d48b3
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 18:43:01 +0200

    Province's class

---
+++ src/Province.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FILE Province.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/12/2021 - 4:19 PM
+ *
+ * @package DRCData;
+ */
+
+namespace DRCData;
+
+
+class Province
+{
+    public function __construct()
+    {
+    }
+}
