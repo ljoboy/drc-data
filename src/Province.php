601ef0c39ba34aed79b235241dda81f12f3e1b1f
Dark Angel <jonathanyombo@gmail.com>
2021-03-13 00:10:20 +0200

    reformat to use Data

--- src/Province.php
+++ src/Province.php
@@ -10,27 +10,19 @@
 
 namespace DRCData;
 
-
-use DRCData\Utils\Text;
 use stdClass;
 
-class Province
+/**
+ * Class Province
+ * @package DRCData
+ */
+class Province extends Data
 {
-    private $provinces;
-    private $importer;
 
     public function __construct()
     {
-        $this->importer = new DataImporter();
-        $this->provinces = $this->importer->provinces();
-    }
-
-    /**
-     * @return array
-     */
-    public function getAll(): array
-    {
-        return $this->provinces;
+        parent::__construct();
+        $this->datas = $this->importer->provinces();
     }
 
     /**
@@ -48,63 +40,4 @@ class Province
         return null;
     }
 
-    /**
-     * @param string $name
-     * @return stdClass|null
-     */
-    public function getByName(string $name): ?StdClass
-    {
-        foreach ($this->provinces as $province) {
-            if ($province->name === $name) {
-                return $province;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * @return stdClass
-     */
-    public function random(): StdClass
-    {
-        return $this->provinces[rand(0, count($this->provinces))];
-    }
-
-    /**
-     * @param string $name
-     * @param bool $isCaseInsensitive
-     * @param bool $withAccent
-     * @return array
-     */
-    public function search(string $name,bool $isCaseInsensitive = true, bool $withAccent = true): array
-    {
-        $provinces = [];
-
-        if ($withAccent === true) {
-            $name = Text::replaceAccents($name);
-        }
-
-        if ($isCaseInsensitive === true) {
-            $name = mb_strtolower($name);
-        }
-
-        foreach ($this->provinces as $province) {
-            $province_name = $province->name;
-
-            if ($withAccent === true) {
-                $province_name = Text::replaceAccents($province_name);
-            }
-
-            if ($isCaseInsensitive === true) {
-                $province_name = mb_strtolower($province_name);
-            }
-
-            if (strpos($province_name, $name) !== false) {
-                $provinces[] = $province;
-            }
-        }
-
-        return $provinces;
-    }
 }
