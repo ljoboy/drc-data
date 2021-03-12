e87f5ee20a01121ddf80fce80e75e14f507339ea
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 17:58:37 +0200

    replace Accent static function

--- src/Utils/Text.php
+++ src/Utils/Text.php
@@ -13,6 +13,13 @@ namespace DRCData\Utils;
 
 class Text
 {
+    static function replaceAccents(string $string): string
+    {
 
+        $search = explode(",", "ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,ø,Ø,Å,Á,À,Â,Ä,È,É,Ê,Ë,Í,Î,Ï,Ì,Ò,Ó,Ô,Ö,Ú,Ù,Û,Ü,Ÿ,Ç,Æ,Œ");
 
+        $replace = explode(",", "c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,o,O,A,A,A,A,A,E,E,E,E,I,I,I,I,O,O,O,O,U,U,U,U,Y,C,AE,OE");
+
+        return str_replace($search, $replace, $string);
+    }
 }
