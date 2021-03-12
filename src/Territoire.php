6fa61c46702c4f57789a4aa5ac1beb9c62b58341
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 18:43:29 +0200

    Territore's class

---
+++ src/Territoire.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FILE Territoire.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/12/2021 - 4:20 PM
+ *
+ * @package DRCData;
+ */
+
+namespace DRCData;
+
+
+class Territoire
+{
+    public function __construct()
+    {
+    }
+}
