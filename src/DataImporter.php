ffdaa88bf891a267c56633572684f64dace4f717
Dark Angel <jonathanyombo@gmail.com>
2021-03-12 18:01:40 +0200

    class for importing data

---
+++ src/DataImporter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * FILE DataImporter.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/12/2021 - 5:04 PM
+ *
+ * @package DRCData;
+ */
+
+namespace DRCData;
+
+
+class DataImporter
+{
+
+}
