2862ef8daa1ce7866a1ba4dab8fb5dc4b02d01c8
Dark Angel <jonathanyombo@gmail.com>
2021-03-15 22:08:19 +0200

    add TextTest class ==> PHPUnit for Text class

---
+++ tests/Utils/TextTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * FILE TextTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/15/2021 - 10:06 PM
+ *
+ * @package Utils;
+ */
+
+namespace Utils;
+
+use DRCData\Utils\Text;
+use PHPUnit\Framework\TestCase;
+
+class TextTest extends TestCase
+{
+
+}
