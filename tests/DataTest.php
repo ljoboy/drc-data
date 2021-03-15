5c639c8453b0b05b637a7318d6b06bae2fd13db4
Dark Angel <jonathanyombo@gmail.com>
2021-03-15 22:03:03 +0200

    add DataTest class ==> PHPUnit for Data class

---
+++ tests/DataTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FILE DataTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/15/2021 - 9:54 PM
+ */
+
+use DRCData\Data;
+use PHPUnit\Framework\TestCase;
+
+class DataTest extends TestCase
+{
+
+}
