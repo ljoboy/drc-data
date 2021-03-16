3ddbe8169d660d363c4eb1fc00d02aca9bd89c66
Dark Angel <jonathanyombo@gmail.com>
2021-03-16 12:54:01 +0200

    add more assertion

--- tests/Utils/TextTest.php
+++ tests/Utils/TextTest.php
@@ -18,15 +18,20 @@ class TextTest extends TestCase
     public function testWordWithAccent()
     {
         $this->assertEquals("eleve", Text::replaceAccents("élève"));
+        $this->assertEquals("Coeur", Text::replaceAccents("Cœur"));
+        $this->assertEquals("HETEROGENEITE", Text::replaceAccents("HÉTÉROGÉNÉITÉ"));
     }
 
     public function testWordWithoutAccent()
     {
         $this->assertEquals('girlfriend', Text::replaceAccents('girlfriend'));
+        $this->assertEquals('Waterproof', Text::replaceAccents('Waterproof'));
+        $this->assertEquals('STATE', Text::replaceAccents('STATE'));
     }
 
-    public function testWithInteger()
+    public function testWithNumber()
     {
-        $this->assertEqualsCanonicalizing(123, Text::replaceAccents(123));
+        $this->assertEquals(123, Text::replaceAccents(123));
+        $this->assertEquals(12.3, Text::replaceAccents(12.3));
     }
 }
