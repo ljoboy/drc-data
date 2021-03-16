2080f67a84bf12b53963ae4f34d8a295747b13cd
Dark Angel <jonathanyombo@gmail.com>
2021-03-16 16:25:41 +0200

    DataTest test class for Data class

---
+++ tests/DataTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FILE DataTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/16/2021 - 2:46 PM
+ */
+
+use DRCData\Data;
+use PHPUnit\Framework\TestCase;
+
+class DataTest extends TestCase
+{
+
+}
