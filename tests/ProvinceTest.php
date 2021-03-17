26b9dd25d57fb8ae35871f9289cd67a0dca1f9d7
Dark Angel <jonathanyombo@gmail.com>
2021-03-17 22:13:56 +0200

    ProvinceTest test class for Province

---
+++ tests/ProvinceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FILE ProvinceTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/16/2021 - 4:30 PM
+ */
+
+use DRCData\Province;
+use PHPUnit\Framework\TestCase;
+
+class ProvinceTest extends TestCase
+{
+
+}
