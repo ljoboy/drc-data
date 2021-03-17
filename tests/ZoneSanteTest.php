b3a005aec78afb122c3266409f7a1318a3c037eb
Dark Angel <jonathanyombo@gmail.com>
2021-03-17 22:11:10 +0200

    ZoneSanteTest test class for ZoneSante

---
+++ tests/ZoneSanteTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FILE ZoneSanteTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/16/2021 - 4:31 PM
+ */
+
+use DRCData\ZoneSante;
+use PHPUnit\Framework\TestCase;
+
+class ZoneSanteTest extends TestCase
+{
+
+}
