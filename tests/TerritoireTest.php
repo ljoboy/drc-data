207b6eb8baf54cc0cb86b879330d9ad0abacf749
Dark Angel <jonathanyombo@gmail.com>
2021-03-17 22:12:31 +0200

    TerritoireTest test class for Territoire

---
+++ tests/TerritoireTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FILE TerritoireTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/16/2021 - 4:30 PM
+ */
+
+use DRCData\Territoire;
+use PHPUnit\Framework\TestCase;
+
+class TerritoireTest extends TestCase
+{
+
+}
