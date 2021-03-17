a4c63a647a9ae835d8787bd55680513b41881aa4
Dark Angel <jonathanyombo@gmail.com>
2021-03-17 22:12:02 +0200

    VilleTest test class for Ville

---
+++ tests/VilleTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FILE VilleTest.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 3/16/2021 - 4:30 PM
+ */
+
+use DRCData\Ville;
+use PHPUnit\Framework\TestCase;
+
+class VilleTest extends TestCase
+{
+
+}
